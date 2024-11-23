26f1e481306b49e62572157494e8dca7c0da5104
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-23 19:24:51 +0530

    route fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
-import data from "./Data"; // Assuming this contains the products array
+import data from "./Data";
 
 function App() {
   return (
@@ -13,29 +13,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
-            {/* You can add more routes here for other screens like product details, etc. */}
           </Routes>
-          <h1>List Product</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <a className="sa" href={`/product/${product.slug}`}>
-                    <p>
-                      <strong className="price">${product.price}</strong>
-                    </p>
-                  </a>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
+        
         </main>
       </div>
     </BrowserRouter>
--- Frontend/src/HomeScreen.jsx
+++ Frontend/src/HomeScreen.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 function HomeScreen() {
   return (
     <div>
-      Home Screen
+       <h1>List Product</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <a className="sa" href={`/product/${product.slug}`}>
+                    <p>
+                      <strong className="price">${product.price}</strong>
+                    </p>
+                  </a>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            ))}
+          </div>
     </div>
   )
 }
