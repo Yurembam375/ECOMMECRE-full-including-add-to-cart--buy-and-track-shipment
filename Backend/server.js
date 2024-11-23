22d57b2e7d71b389634c1f12665ea5d10b2f981b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-23 20:10:26 +0530

    data add

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+
+app.get('/api/products',(req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
