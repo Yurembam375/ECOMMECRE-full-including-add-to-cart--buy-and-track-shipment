350c41ea554bf6058611eaf2d9f75c7b4d9a67a1
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-23 10:53:59 +0530

    okay

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,42 +1,43 @@
 import React from "react";
-import data from "./Data";
-import BrowserRouter, { Route, Routes } from'react-router-dom';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
+import data from "./Data"; // Assuming this contains the products array
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-        <a href="/">Laitonjam Collection</a>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomeScreen/>}></Route>
-        <h1>List Product</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}></img>
-              </a>
-              <div className="product-info">
+      <div>
+        <header>
+          <a href="/">Laitonjam Collection</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            {/* You can add more routes here for other screens like product details, etc. */}
+          </Routes>
+          <h1>List Product</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
                 <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
+                  <img src={product.image} alt={product.name} />
                 </a>
-                <a className="sa" href={`/product/${product.slug}`}>
-                  <p>
-                    <strong className="price">${product.price}</strong>
-                  </p>
-                </a>
-                <button>Add to Cart</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <a className="sa" href={`/product/${product.slug}`}>
+                    <p>
+                      <strong className="price">${product.price}</strong>
+                    </p>
+                  </a>
+                  <button>Add to Cart</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        </Routes>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
