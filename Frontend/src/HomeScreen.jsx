03e6d5ac04e5bea9c3bf8013df00c61ed288720c
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-24 12:10:42 +0530

    okay for networking

--- Frontend/src/HomeScreen.jsx
+++ Frontend/src/HomeScreen.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
-import data from './Data'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]); // Corrected state destructuring
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("http://localhost:4000/api/products");
+        setProducts(result.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-       <h1>List Product</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
+      <h1>List of Products</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
 
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <Link className="sa" to={`/product/${product.slug}`}>
-                    <p>
-                      <strong className="price">${product.price}</strong>
-                    </p>
-                  </Link>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            ))}
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <Link className="sa" to={`/product/${product.slug}`}>
+                <p>
+                  <strong className="price">${product.price}</strong>
+                </p>
+              </Link>
+              <button>Add to Cart</button>
+            </div>
           </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
