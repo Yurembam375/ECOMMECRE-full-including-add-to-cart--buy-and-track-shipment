1b92f0bcf5de1fab26462c44fb005747f0a8b1da
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-25 21:58:12 +0530

    rating add

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useReducer } from "react";
 import { useEffect } from "react";
-
+import Rating from './Rating';
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
