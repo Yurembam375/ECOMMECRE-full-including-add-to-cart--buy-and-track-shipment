847bf76f61a98b283bb57742c94a7884eb9a3a71
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-25 21:39:53 +0530

    poka

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useReducer } from "react";
+import { useEffect } from "react";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 function ProductScreen() {
   const { slug } = useParams(); // Destructure slug from params
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(
+          `http://localhost:4000/api/products/slug/${slug}`
+        );
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
+      }
+    };
+    fetchData();
+  }, [slug]);
   return (
     <div>
       <h1>{slug}</h1>
