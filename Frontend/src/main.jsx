e5d46639781800cd803118b8aa808c1e4cf179be
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-27 20:46:41 +0530

    Wrap the App and Helmet inside the Store

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async'; // Corrected the import
+import { StoreProvider } from './Store.jsx';
 
 const root = createRoot(document.getElementById('root')); // Ensure the root element is properly selected
 
 root.render(
   <StrictMode>
+    <StoreProvider>
     <HelmetProvider>
       <App />
     </HelmetProvider>
+    </StoreProvider>
+  
   </StrictMode>
 );
