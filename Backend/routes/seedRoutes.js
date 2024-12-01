2758199cbf46b42453735a9cfa2baaebc1ea003f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-01 20:34:31 +0530

    user model and seed created

--- Backend/routes/seedRoutes.js
+++ Backend/routes/seedRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js'; // Assuming `data` is imported from a file where `products` is defined.
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
     await Product.deleteMany({});  // Use deleteMany instead of remove
     const createdProducts = await Product.insertMany(data.products);
-    res.status(201).json({ message: 'Products seeded successfully', createdProducts });
+    await User.deleteMany({});  // Use deleteMany instead of remove
+    const createdUser = await User.insertMany(data.users);
+    res.status(201).json({ message: 'Products seeded successfully', createdProducts,createdUser });
 });
 
 export default seedRouter;
