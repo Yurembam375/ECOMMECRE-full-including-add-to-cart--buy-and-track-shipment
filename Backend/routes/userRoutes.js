3fd8a26d708648125250ffacf4554847dbc1639f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-01 21:20:53 +0530

    okay

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import express from "express";
+import User from "../models/userModel";
 
-const UserRouter=express.Router();
+const UserRouter = express.Router();
 
 UserRouter.post(
-    '/sigin',
-    expressAsyncHandler(async(req,res)=>{
-
-    })
-)
+  "/sigin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
